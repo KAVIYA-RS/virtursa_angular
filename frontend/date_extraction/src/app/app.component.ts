07f89398719c82c9010c9bf91dcbb90d47bdbbea
KAVIYA R S <104513432+KAVIYA-RS@users.noreply.github.com>
2022-11-23 23:04:14 +0530

    Add files via upload

---
+++ frontend/date_extraction/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  Today:Date=new Date();
+  Local:string=new Date().toLocaleString();
+}
